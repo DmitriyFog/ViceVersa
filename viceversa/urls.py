a0a4482ab1216d152d9f7993cdcb30d210bca15c
Dmitriy <puchkov.arkan@yandex.ru>
2020-08-16 12:09:56 +0300

    Add reverse feature

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -17,6 +17,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('about/', views.about),
     path('', views.home),
+    path('reverse/', views.reverse, name='reverse'),
 ]
+
